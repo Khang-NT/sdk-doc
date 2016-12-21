d6e9b3fe7e40b75055126024aa29abd2585de764
Khang Nguyen <khang.neon.1997@gmail.com>
2016-12-21 19:40:59 +0700

    Fix gitinore caused error

--- generate.sh
+++ generate.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
 export PATH=$PATH:~/.composer/vendor/bin
+mkdir -p generate/static
 daux generate --format=html --source=docs --destination=generate/static
 for f in $(find generate/static -name '*.html'); do python group_code_block.py "$f"; done
